d3a030119d4b70362e4b0a991cc945d82753c02b
khoi <dinhvinhkhoi@gmail.com>
2023-03-08 15:35:21 +0700

    header

--- ecommerce-app/src/components/Header.jsx
+++ ecommerce-app/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { BsSearch } from "react-icons/bs";
 const Header = () => {
   return (
@@ -8,9 +8,7 @@ const Header = () => {
         <div className="container">
           <div className="row">
             <div className="col-6">
-              <p className="text-white mb-0">
-                Miễn phí giao hàng và hoàn trả cho đơn hàng trên 2.000.000
-              </p>
+              <p className="text-white mb-0">Free shipping</p>
             </div>
             <div className="col-6">
               <p className="text-end text-white mb-0">
@@ -45,7 +43,93 @@ const Header = () => {
                 </span>
               </div>
             </div>
-            <div className="col-5"></div>
+            <div className="col-5">
+              <div className="header-upper-links d-flex align-items-center justify-content-between">
+                <div>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                    <img src="images/compare.svg" alt="compare" />
+                    <p>
+                      Compare <br /> Product
+                    </p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                    <img src="images/wishlist.svg" alt="wishlist" />
+                    <p>
+                      Favourite <br /> Wishlist
+                    </p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                    <img src="images/user.svg" alt="user" />
+                    <p>
+                      Login <br /> My Account
+                    </p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                    <img src="images/cart.svg" alt="cart" />
+                    <div className="d-flex flex-column gap-10">
+                      <span className="badge bg-white  text-dark">0</span>
+                      <p>$500</p>
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      <header className="header-bottom py-3">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <div className="menu-bottom d-flex align-items-center gap-30">
+                <div>
+                  <div className="dropdown">
+                    <button
+                      className="btn btn-secondary dropdown-toggle bg-transparent border-0 d-flex gap-10 align-items-center"
+                      type="button"
+                      id="dropdownMenuButton1"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      <img src="/images/menu.svg" alt="" />
+                      <span>Shop Categories</span>
+                    </button>
+                    <ul
+                      className="dropdown-menu py-0"
+                      aria-labelledby="dropdownMenuButton1"
+                    >
+                      <li>
+                        <Link className="dropdown-item text-white" to="">
+                          Action
+                        </Link>
+                      </li>
+                      <li>
+                        <Link className="dropdown-item text-white" to="">
+                          Another action
+                        </Link>
+                      </li>
+                      <li>
+                        <Link className="dropdown-item text-white" to="">
+                          Something else here
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="menu-links d-flex gap-15 ">
+                  <NavLink to="/">Home</NavLink>
+                  <NavLink to="/">Our Store</NavLink>
+                  <NavLink to="/">Blogs</NavLink>
+                  <NavLink to="/contact">Contact</NavLink>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </header>
